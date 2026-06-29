b41d21dc33233c0e83915c4dbb7eaa8e00f76e4a
MD SHAHID RAZA <shahidrazaraza77@gmail.com>
2026-06-30 00:18:33 +0530

    Update 0231-power-of-two.cpp

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -4,3 +4,23 @@ public:
         return n > 0 && (n & (n - 1)) == 0;
     }
 };
+
+
+
+// optimal method 
+
+
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+
+        if (n <= 0)
+            return false;
+
+        while (n % 2 == 0) {
+            n = n / 2;
+        }
+
+        return n == 1;
+    }
+};
