3916bd772d9d2542c864d3260f69698057a8d8b5
MD SHAHID RAZA <shahidrazaraza77@gmail.com>
2026-09-14 17:13:30 +0530

    Time: 0 ms (100%), Space: 10.5 MB (52.25%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -2,26 +2,31 @@ class Solution {
 public:
     string reverseWords(string s) {
 
-        int n= s.length();
-        string ans= "";
+        int n = s.length();
+        string ans = "";
 
-        reverse (s.begin(),s.end());
+        // Step 1: Reverse the complete string
+        reverse(s.begin(), s.end());
 
-        for (int i=0;i<n;i++){
-            string word="";
-            while(i<n && s[i]!=' '){
-                word +=s[i];
+        for (int i = 0; i < n; i++) {
+            string word = "";
+
+            // Step 2: Extract one word
+            while (i < n && s[i] != ' ') {
+                word += s[i];
                 i++;
-
             }
-            reverse(word.begin(),word.end());
 
-            if (word.length()>0){
-                ans+=" "+ word;
+            // Step 3: Reverse the extracted word
+            reverse(word.begin(), word.end());
 
+            // Step 4: Add the word to answer
+            if (word.length() > 0) {
+                ans += " " + word;
             }
         }
+
+        // Step 5: Remove the first extra space
         return ans.substr(1);
-        
     }
 };
